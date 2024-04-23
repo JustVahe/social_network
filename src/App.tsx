beb34ad24480fd36301de8e8638cd624faf6089e
JustVahe <111753033+JustVahe@users.noreply.github.com>
2024-04-23 14:26:38 +0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import MenuLeftButtons from "./components/MenuLeftButtons"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <MenuLeftButtons />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MenuLeftButtons.tsx
@@ -0,0 +1,7 @@
+export default function MenuLeftButtons() {
+  return (
+    <div className="w-[70px] h-svh bg-white fixed z-10 shadow-xl shadow-zinc-300">
+
+    </div>
+  )
+}
