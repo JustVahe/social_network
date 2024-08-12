057ed891e0887b27384b561f20deb796cd63887c
JustVahe <vlogodes.design@gmail.com>
2024-08-12 15:41:20 +0400

    Fix

--- server/dist/src/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { AuthorizationController } from "../controllers/authorization.controller.ts";
-const authRouter = Router();
-const authController = new AuthorizationController();
-authRouter.post("/login", authController.login.bind(authController));
-authRouter.post("/register", authController.register.bind(authController));
-authRouter.post("/refresh", authController.refreshToken.bind(authController));
-export default authRouter;
--- server/dist/src/utils/functions/fileFilter.js
+++
@@ -1,9 +0,0 @@
-export const imageFilter = (req, file, callback) => {
-    const [type] = file.mimetype.split("/");
-    if (type === "image") {
-        callback(null, true);
-    }
-    else if (type !== "image") {
-        return callback(null, false);
-    }
-};
