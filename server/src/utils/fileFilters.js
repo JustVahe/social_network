cf62fa4056cf62cb17ed475ba31b8b1f0dc42705
JustVahe <vlogodes.design@gmail.com>
2024-08-12 14:33:53 +0400

    Rearrangment and deployment

--- server/src/utils/fileFilters.js
+++
@@ -1,9 +0,0 @@
-module.exports.imageFilter = (request, file, callback) => {
-    
-    const [type] = file.mimetype.split("/")
-    if (type === "image") {
-        callback(null,true);
-    } else if (type !== "image") {
-        return callback(new Error("File Validation Error"), false);
-    }
-};
--- server/src/utils/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const url = process.env.SUPABASE_URL;
-const key = process.env.SUPABASE_KEY;
-const supabase = createClient(url, key);
-
-module.exports = supabase;
